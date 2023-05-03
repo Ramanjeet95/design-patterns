908a8cf563ea5bc3c1d6701698376926c08ef738
akashchaudhary <akashsorout@gmail.com>
2023-05-03 23:03:45 +0530

    decorator pattern | baseclient -> loggingclient -> retryclient

---
+++ src/main/java/com/dp/behaviour/decorator/RetryClient.java
@@ -0,0 +1,36 @@
+package com.dp.behaviour.decorator;
+
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
+
+@Slf4j
+public class RetryClient implements RestClient {
+
+    private RestClient restClient;
+    private Function<HttpRequest, HttpResponse<String>> callRestClient;
+    public RetryClient(RestClient restClient) {
+        this.restClient = restClient;
+        RetryConfig retryConfig = RetryConfig.custom()
+                .maxAttempts(2)
+                .waitDuration(Duration.ofMillis(100))
+                .retryOnResult(response -> ((HttpResponse<String>)response).statusCode() == 500)
+                .retryOnException(e -> e instanceof RuntimeException)
+                .retryExceptions(IOException.class, TimeoutException.class)
+//                .ignoreExceptions(BusinessException.class, OtherBusinessException.class)
+                .build();
+        callRestClient = Retry.decorateFunction(Retry.of("Call Rest Client", retryConfig), restClient::send);
+    }
+
+    @Override
+    public HttpResponse<String> send(HttpRequest httpRequest) {
+        return callRestClient.apply(httpRequest);
+    }
+}
---
+++ src/main/java/com/dp/behaviour/decorator/TestDecoratedClient.java
@@ -0,0 +1,13 @@
+package com.dp.behaviour.decorator;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+
+public class TestDecoratedClient {
+    public static void main(String[] args) throws URISyntaxException {
+
+        RestClient restClient = new RetryClient(new LoggingClient(new BaseClient()));
+        restClient.send(HttpRequest.newBuilder().GET().uri(new URI("https://google.com")).build());
+    }
+}
